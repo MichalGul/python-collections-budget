e07f85a7240c02b07e3aa94ff622a720b3c97a24
Michał <mwarioxin@gmail.com>
2021-10-14 19:40:05 +0200

    Added ploting expense category

--- budget/BudgetList.py
+++ budget/BudgetList.py
@@ -1,4 +1,6 @@
 from . import Expense
+import matplotlib.pyplot as plt
+
 
 class BudgetList():
     def __init__(self, budget):
@@ -11,6 +13,19 @@ class BudgetList():
     def __len__(self):
         return len(self.expenses) + len(self.overages)
 
+    # create budget list iterator
+    def __iter__(self):
+        self.iter_e = iter(self.expenses)
+        self.iter_o = iter(self.overages)
+        return self
+    # define next methods to itarate over BudgetList
+    def __next__(self):
+        try:
+            return self.iter_e.__next__()
+        except StopIteration as stop:
+            print("Over budget")
+            return self.iter_o.__next__()
+
     def append(self, item):
         if self.sum_expenses + item < self.budget:
             self.expenses.append(item)
@@ -21,14 +36,24 @@ class BudgetList():
 
 
 def main():
-    myBudgetList = BudgetList(1200)
+    myBudgetList = BudgetList(30000)
     expenses = Expense.Expenses()
     expenses.read_expenses('data/monefy-2021-10-11_06-11-44.csv')
-
+    print(expenses.categories)
     for expense in expenses.list:
         myBudgetList.append(expense.amount)
-    print('The count of all expenses: ' + {str(len(myBudgetList))})
+    print('The count of all expenses: ' + str(len(myBudgetList)))
 
+    for entry in myBudgetList:
+        print("budget entry", entry)
+
+    #create plot foe expenses
+    fig, ax = plt.subplots()
+    labels = ['Expenses', 'Overages', 'Budget']
+    values = [myBudgetList.sum_expenses, myBudgetList.sum_overages, myBudgetList.budget]
+    ax.bar(labels, values, color=['green', 'red', 'blue'])
+    ax.set_title("Your total expenses vs. total budget")
+    plt.show()
 
 if __name__ == "__main__":
     main()
--- budget/Expense.py
+++ budget/Expense.py
@@ -1,6 +1,7 @@
 import csv
 from datetime import datetime
 
+
 class Expense():
     def __init__(self, date_str, vendor, category, amount):
         self.date_time = datetime.strptime(date_str, '%d.%m.%Y')
@@ -8,38 +9,51 @@ class Expense():
         self.category = category
         self.amount = amount
 
+    def __repr__(self):
+        return f"{str(self.category)} - {str(self.amount)}"
 
 class Expenses():
     def __init__(self):
         self.list = []
         self.sum = 0
+        self.categories = set()
 
     # Read in the December spending data, row[2] is the $$, and need to format $$
-    def read_expenses(self,filename):
+    def read_expenses(self, filename):
         with open(filename, newline='', encoding='utf-8') as csvfile:
-            csvreader = csv.reader(csvfile, delimiter=';',)
+            csvreader = csv.reader(csvfile, delimiter=';', )
             for row in csvreader:
                 if '-' not in row[3]:
                     continue
-                print(row[3])
+                # print(row[3])
                 # print(float((row[3][2:])))
-                amount = float((row[3][1:]).replace(',','.'))
-                print(amount)
+                amount = float((row[3][1:]).replace(',', '.'))
+                # print(amount)
                 self.list.append(Expense(row[0], row[1], row[2], amount))
+                self.categories.add(row[2])
                 self.sum += amount
 
     def categorize_for_loop(self):
-            necessary_expenses = set()
-            food_expenses = set()
-            unnecessary_expenses = set()
-            for i in self.list:
-                if (i.category == 'Phone'    or i.category == 'Auto and Gas' or 
-                    i.category == 'Classes'  or i.category == 'Utilities' or 
-                    i.category == 'Mortgage'): 
-                    necessary_expenses.add(i)
-                elif(i.category == 'Groceries' or i.category == 'Eating Out'):
-                    food_expenses.add(i)
-                else:
-                    unnecessary_expenses.add(i)
-            
-            return [necessary_expenses, food_expenses, unnecessary_expenses]
+        necessary_expenses = set()
+        food_expenses = set()
+        unnecessary_expenses = set()
+        for i in self.list:
+            if i.category in ("Rachunki", "Dom", "Samochód", "Drogerie", "Zdrowie", 'Sport'):
+                necessary_expenses.add(i)
+            elif i.category in ("Zakupy Żabka", "Zakupy Duże", "Pieczywo", "Warzywa", 'Jedzenie na mieście') :
+                food_expenses.add(i)
+            else:
+                unnecessary_expenses.add(i)
+
+        return [necessary_expenses, food_expenses, unnecessary_expenses]
+
+    def categorize_set_comprehension(self):
+        necessary_expenses = {x for x in self.list if x.category in ("Rachunki",
+                                                                     "Dom",
+                                                                     "Samochód", "Drogerie", "Zdrowie", 'Sport', "Corsinka"
+                                                                     )}
+        food_expenses = {x for x in self.list if
+                         x.category in ("Zakupy Żabka", "Zakupy Duże", "Pieczywo", "Warzywa", 'Jedzenie na mieście')}
+        unnecessary_expenses = set(self.list) - necessary_expenses - food_expenses
+
+        return [necessary_expenses, food_expenses, unnecessary_expenses]
